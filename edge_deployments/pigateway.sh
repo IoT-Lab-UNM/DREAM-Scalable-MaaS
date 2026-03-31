37427f86f7754648fe789e1b89c93b335afb3348
HenokBerhanu <henokberhanut@gmail.com>
2026-03-31 16:52:30 -0600

    update pigatewa

--- edge_deployments/pigateway.sh
+++ edge_deployments/pigateway.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 KUBEEDGE_VERSION="v1.23.0"
 CLOUDNODE_IP="10.12.10.124"
-TOKEN="e84aa4f079426b66718898f74c0490c6422d0e1099b68e331893f9f78dc34238.eyJhbGciOiJIUzI1NLIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NzUwNjg2NjR9.K4cgBXOvkcRgn71eq1Pb6wbTzu3moONswu3t9Z632Vc"
+TOKEN="e84aa4f079426b66718898f74c0490c6422d0e1099b68e331893f9f78dc34238.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NzUwNjg2NjR9.K4cgBXOvkcRgn71eq1Pb6wbTzu3mo0Nswu3t9Z632Vc"
 
 echo "=== Preparing containerd on pigateway ==="
 sudo mkdir -p /etc/containerd
